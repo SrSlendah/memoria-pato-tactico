8129484f4d6080ee5415cb3f108c9d3190541bbc
SrSlendah <slenderymas@gmail.com>
2023-06-15 02:08:48 +0200

    v4.2.3

---
+++ js/admin-changelog.js
@@ -0,0 +1,102 @@
+function GetVersion() {
+    var version = document.getElementById("versiones").value
+
+
+    if (version == 91) {
+        document.getElementById("v91").style.display = '';
+        document.getElementById("v90").style.display = 'none';
+        document.getElementById("v89").style.display = 'none';
+        document.getElementById("v88").style.display = 'none';
+        document.getElementById("v87").style.display = 'none';
+        document.getElementById("v86").style.display = 'none';
+        document.getElementById("v85").style.display = 'none';
+        document.getElementById("v84").style.display = 'none';
+        document.getElementById("v83").style.display = 'none';
+        document.getElementById("v82").style.display = 'none';
+        document.getElementById("v81").style.display = 'none';
+        document.getElementById("v80").style.display = 'none';
+        document.getElementById("v79").style.display = 'none';
+        document.getElementById("v78").style.display = 'none';
+        document.getElementById("v77").style.display = 'none';
+        document.getElementById("v76").style.display = 'none';
+        document.getElementById("v75").style.display = 'none';
+        document.getElementById("v74").style.display = 'none';
+        document.getElementById("v73").style.display = 'none';
+        document.getElementById("v72").style.display = 'none';
+        document.getElementById("v71").style.display = 'none';
+        document.getElementById("v70").style.display = 'none';
+        document.getElementById("v69").style.display = 'none';
+        document.getElementById("v68").style.display = 'none';
+        document.getElementById("v67").style.display = 'none';
+        document.getElementById("v66").style.display = 'none';
+        document.getElementById("v65").style.display = 'none';
+        document.getElementById("v64").style.display = 'none';
+        document.getElementById("v63").style.display = 'none';
+        document.getElementById("v62").style.display = 'none';
+        document.getElementById("v61").style.display = 'none';
+        document.getElementById("v60").style.display = 'none';
+        document.getElementById("v59").style.display = 'none';
+        document.getElementById("v58").style.display = 'none';
+        document.getElementById("v57").style.display = 'none';
+        document.getElementById("v56").style.display = 'none';
+        document.getElementById("v55").style.display = 'none';
+        document.getElementById("v54").style.display = 'none';
+        document.getElementById("v53").style.display = 'none';
+        document.getElementById("v52").style.display = 'none';
+        document.getElementById("v51").style.display = 'none';
+        document.getElementById("v50").style.display = 'none';
+        document.getElementById("v49").style.display = 'none';
+        document.getElementById("v48").style.display = 'none';
+        document.getElementById("v47").style.display = 'none';
+        document.getElementById("v46").style.display = 'none';
+        document.getElementById("v45").style.display = 'none';
+        document.getElementById("v44").style.display = 'none';
+        document.getElementById("v43").style.display = 'none';
+        document.getElementById("v42").style.display = 'none';
+        document.getElementById("v41").style.display = 'none';
+        document.getElementById("v40").style.display = 'none';
+        document.getElementById("v39").style.display = 'none';
+        document.getElementById("v38").style.display = 'none';
+        document.getElementById("v37").style.display = 'none';
+        document.getElementById("v36").style.display = 'none';
+        document.getElementById("v35").style.display = 'none';
+        document.getElementById("v34").style.display = 'none';
+        document.getElementById("v33").style.display = 'none';
+        document.getElementById("v32").style.display = 'none';
+        document.getElementById("v31").style.display = 'none';
+        document.getElementById("v30").style.display = 'none';
+        document.getElementById("v29").style.display = 'none';
+        document.getElementById("v28").style.display = 'none';
+        document.getElementById("v27").style.display = 'none';
+        document.getElementById("v26").style.display = 'none';
+        document.getElementById("v25").style.display = 'none';
+        document.getElementById("v24").style.display = 'none';
+        document.getElementById("v23").style.display = 'none';
+        document.getElementById("v22").style.display = 'none';
+        document.getElementById("v21").style.display = 'none';
+        document.getElementById("v20").style.display = 'none';
+        document.getElementById("v19").style.display = 'none';
+        document.getElementById("v18").style.display = 'none';
+        document.getElementById("v17").style.display = 'none';
+        document.getElementById("v16").style.display = 'none';
+        document.getElementById("v15").style.display = 'none';
+        document.getElementById("v14").style.display = 'none';
+        document.getElementById("v13").style.display = 'none';
+        document.getElementById("v12").style.display = 'none';
+        document.getElementById("v11").style.display = 'none';
+        document.getElementById("v10").style.display = 'none';
+        document.getElementById("v9").style.display = 'none';
+        document.getElementById("v8").style.display = 'none';
+        document.getElementById("v7").style.display = 'none';
+        document.getElementById("v6").style.display = 'none';
+        document.getElementById("v5").style.display = 'none';
+        document.getElementById("v4").style.display = 'none';
+        document.getElementById("v3").style.display = 'none';
+        document.getElementById("v2").style.display = 'none';
+        document.getElementById("v1").style.display = 'none';
+
+        console.log("Se ha recibido el valor "+version)
+    } 
+
+}
+
