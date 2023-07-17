e9bb915fe706357c96bc5522e5c5b0bbd9fff743
SrSlendah <slenderymas@gmail.com>
2023-07-18 01:53:34 +0200

    v4.3.0

--- js/anime.js
+++ js/anime.js
@@ -1,15 +1,33 @@
-function showSection(section) {
-    document.getElementById('sec1').style.display = 'none';
-    document.getElementById('sec1-btn').style.backgroundColor = '#6f3c96';
-    document.getElementById('sec1-btn').style.borderColor = '#4b2866';
-    document.getElementById('sec1-btn').style.color = '#fff';
-    document.getElementById('sec2').style.display = 'none';
-    document.getElementById('sec2-btn').style.backgroundColor = '#6f3c96';
-    document.getElementById('sec2-btn').style.borderColor = '#4b2866';
-    document.getElementById('sec2-btn').style.color = '#fff';
-
-    document.getElementById(`sec${section}`).style.display = '';
-    document.getElementById(`sec${section}-btn`).style.backgroundColor = '#4b2966';
-    document.getElementById(`sec${section}-btn`).style.borderColor = '#311a42';
-    document.getElementById(`sec${section}-btn`).style.color = '#9e9e9e';
-}
+function showSection(sectionNumber) {
+    const totalSections = 5; // Actualiza esto con el número total de secciones
+    
+    for (let i = 1; i <= totalSections; i++) {
+      const section = document.getElementById('sec' + i);
+      const button = document.getElementById('sec' + i + '-btn');
+      
+      section.style.display = i === sectionNumber ? '' : 'none';
+      button.style.backgroundColor = i === sectionNumber ? '#4b2966' : '#6f3c96';
+      button.style.borderColor = i === sectionNumber ? '#311a42' : '#4b2866';
+      button.style.color = i === sectionNumber ? '#9e9e9e' : '#fff';
+    }
+  }
+    
+    function showSec1() {
+      showSection(1);
+    }
+    
+    function showSec2() {
+      showSection(2);
+    }
+    
+    function showSec3() {
+      showSection(3);
+    }
+    
+    function showSec4() {
+      showSection(4);
+    }
+    
+    function showSec5() {
+      showSection(5);
+    }
