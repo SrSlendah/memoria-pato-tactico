f0eab46c7936e72aa94f7706ea1dcc077a530495
Slendah <slenderymas@gmail.com>
2023-08-04 19:09:27 +0200

    v4.3.2

--- js/anime.js
+++ js/anime.js
@@ -5,8 +5,13 @@ function abrirOV(overlayNumber) {
   if (overlay) {
     overlay.style.display = '';
     document.body.classList.add('scroll-blocked');
+    console.log("[Overlay] Se ha pulsado el botón de mas información, Se ha abierto el overlay ov" + overlayNumber)
   }
-  console.log("[Overlay] Se ha pulsado el botón de mas información, Se ha abiarto el overlay ov" + overlayNumber)
+  else {
+    console.warn("[Overlay] Se ha pulsado el botón de mas información, pero no se ha podido abrir el overlay ov" + overlayNumber)
+  }
+  
+  
 }
 
 function cerrarOV(overlayNumber) {
@@ -14,8 +19,26 @@ function cerrarOV(overlayNumber) {
   if (overlay) {
     overlay.style.display = 'none';
     document.body.classList.remove('scroll-blocked');
+    console.log("[Overlay] Se ha pulsado el boton de cerrar overlay, overlay cerrado")
   }
-  console.log("[Overlay] Se ha pulsado el boton de cerrar overlay, overlay cerrado")
+  else {
+    console.warn("[Overlay] Se ha pulsado el botón de cerrar overlay, pero no se ha podido cerrar el overlay ov" + overlayNumber)
+  }
+}
+
+function OvVisible() {
+  var overlayNumber = 1;
+  var overlay = document.getElementById('ov' + overlayNumber);
+
+  while (overlay) {
+    if (overlay.style.display !== 'none') {
+      return true;
+    }
+    overlayNumber++;
+    overlay = document.getElementById('ov' + overlayNumber);
+  }
+
+  return false;
 }
 
 function cerrarOVs() {
@@ -31,9 +54,12 @@ function cerrarOVs() {
 }
 
 document.addEventListener('keydown', function(event) {
-  if (event.key === "Escape") {
+  if (event.key === "Escape" && OvVisible()) {
     console.log("[Overlay] Se ha pulsado la tecla escape, overlay cerrado")
     cerrarOVs();
+  }
+  else {
+    console.warn("[Overlay] Se ha pulsado la tecla escape sin haber ningun overlay activo, no ha habido ningun cambio")
   }
 });
 
--- js/redirect.js
+++ js/redirect.js
@@ -3,13 +3,21 @@ function goTo(string) {
 // Manga
 
     if (string == 'manga_jojo') {
-        window.location.href = "../lista-manga/jojos-bizarre-adventure.html"
+        window.location.href = "../lista-manga/jojos-bizarre-adventure"
+    };
+
+    if (string == 'manga_nanatsu') {
+        window.location.href = "../lista-manga/nanatsu-no-taizai"
     };
 
 //Anime
 
     if (string == 'anime_jojo') {
-        window.location.href = "../lista-anime/jojos-bizarre-adventure.html"
+        window.location.href = "../lista-anime/jojos-bizarre-adventure"
+    };
+
+    if (string == 'anime_nanatsu') {
+        window.location.href = "../lista-anime/nanatsu-no-taizai"
     };
 
 //Wiki Brawlhalla
