5e98b4e13aca90acac21e929134eef4dde6dc5fb
SrSlendah <slenderymas@gmail.com>
2023-10-02 17:16:35 +0200

    v4.3.3

--- js/wbh_cheat-sheets.js
+++ js/wbh_cheat-sheets.js
@@ -1,14 +1,28 @@
-function GetArma1() {
-    const selectElement = document.getElementById("arma1");
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-    const imageUrl = selectedOption.getAttribute("data-image");
-    const imagenSeleccionada = document.getElementById("imagenSeleccionada");
-  
-    if (imageUrl) {
-      imagenSeleccionada.style.display = "block";
-      imagenSeleccionada.querySelector("img").src = imageUrl;
-    } else {
-      imagenSeleccionada.style.display = "none";
-    }
-  }
-  
+
+function filterTables() {
+  var selectedArma = document.getElementById('arma').value;
+  var selectedFiltro = document.getElementById('filtro').value;
+
+  var tables = document.querySelectorAll('table[data-tags]');
+  tables.forEach(function(table) {
+      var tags = table.getAttribute('data-tags').split(',');
+      var showTable = true;
+
+      if (selectedArma !== '' && tags.indexOf(selectedArma) === -1) {
+          showTable = false;
+      }
+
+      if (selectedFiltro !== '' && tags.indexOf(selectedFiltro) === -1) {
+          showTable = false;
+      }
+
+      if (showTable) {
+          table.style.display = 'table';
+      } else {
+          table.style.display = 'none';
+      }
+  });
+}
+
+// Llamar a filterTables() inicialmente para mostrar las tablas basadas en los valores iniciales de los select.
+filterTables();
