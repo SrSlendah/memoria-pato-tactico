64589e05fcbbfc7f8f955056172b24bf5bce3ec9
Slendah <slenderymas@gmail.com>
2024-07-29 22:52:02 +0200

    v4.3.6

--- js/wbh_cheat-sheets.js
+++ js/wbh_cheat-sheets.js
@@ -1,24 +1,3 @@
-function scrollToSection(sectionId) {
-    var element = document.getElementById(sectionId);
-    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
-
-// Función para mostrar u ocultar el menú de navegación
-function toggleNavMenu() {
-    var navMenu = document.getElementById('nav-menu');
-    var buttonsContainer = document.querySelector('.secciones');
-    var buttonsRect = buttonsContainer.getBoundingClientRect();
-
-    if (buttonsRect.bottom <= 0) {
-        navMenu.style.display = 'block';
-    } else {
-        navMenu.style.display = 'none';
-    }
-}
-
-// Escuchar el evento scroll para mostrar u ocultar el menú de navegación
-window.addEventListener('scroll', toggleNavMenu);
-
 // Facilitar misiones
 
 function filterTables() {
