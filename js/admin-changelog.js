7a3256f90c6146291b4778d9425663f4ab873edc
Slendah <56037403+SrSlendah@users.noreply.github.com>
2024-08-04 15:15:03 +0200

    v4.4.0

--- js/admin-changelog.js
+++ js/admin-changelog.js
@@ -1,19 +1,20 @@
 function GetVersion() {
-    // Obtener el valor seleccionado del menú desplegable
     let version = document.getElementById("versiones").value;
-    let elemento = document.getElementById(version);  // ID del elemento a mostrar
+    let elemento = document.getElementById(version);
 
-    // Ocultar todos los elementos con ID que no sea "#versiones"
     let elementos = document.querySelectorAll("[id]:not(#versiones)");
-    elementos.forEach(el => el.style.display = "none");
+    elementos.forEach(el => {
+        // Verifica si el ID es numérico
+        if (/^\d+$/.test(el.id)) {
+            el.style.display = "none";
+        }
+    });
 
-    // Mostrar el elemento seleccionado si existe
     if (elemento) {
-        elemento.style.display = "block"; // Asegúrate de que el elemento se muestre
+        elemento.style.display = "block";
     } else {
         console.warn("El elemento con ID '" + version + "' no existe.");
     }
 
-    // Imprimir el valor recibido para depuración
     console.log("Se ha recibido el valor " + version);
 }
