ab6383b209cbb2d538cc765f443157365321d892
SrSlendah <slenderymas@gmail.com>
2025-02-24 12:23:20 +0100

    v4.4.1

--- js/mc_texturepack.js
+++ js/mc_texturepack.js
@@ -13,3 +13,31 @@ function filtrarVersion() {
 }
 
 window.onload = filtrarVersion;
+
+
+function descargar(id, nombre) {
+    
+    const urls = {
+        "25w08a_v1-3": "../media/mc/resourcepacks/random-things/mc_25w08a/v_1-3/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip",
+        "25w08a_v1-2": "../media/mc/resourcepacks/random-things/mc_25w08a/v_1-2/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip",
+        "25w08a_v1-1": "../media/mc/resourcepacks/random-things/mc_25w08a/v_1-1/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip",
+        "25w08a_v1-0": "../media/mc/resourcepacks/random-things/mc_25w08a/v_1-0/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip",
+
+        "1-21-4_v1-3": "../media/mc/resourcepacks/random-things/mc_1-21-4/v_1-3/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip",
+        "1-21-4_v1-2": "../media/mc/resourcepacks/random-things/mc_1-21-4/v_1-2/§kaaa§r§l§d Random Things §0§r§kaaa§r.zip"
+    };
+
+    const url = urls[id];
+
+    if (!url) {
+        console.error("ID no válido");
+        return;
+    }
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = nombre;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
