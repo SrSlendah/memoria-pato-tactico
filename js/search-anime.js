86b5cd584bf0b84220fa52939753c050321ae0d1
SrSlendah <slenderymas@gmail.com>
2025-05-25 18:01:36 +0200

    v4.5.1

--- js/search-anime.js
+++ js/search-anime.js
@@ -33,6 +33,7 @@ function obtenerResultadosConURLs(query) {
     { resultado: 'Go-Tōbun no Hanayome', url: '../lista-anime/go-tobun-no-hanayome' },
     { resultado: 'Goblin Slayer', url: '../lista-anime/goblin-slayer' },
     { resultado: 'Gokushufudou', url: '../lista-anime/gokushufudou' },
+    { resultado: 'High-Rise Invasion', url: '../lista-anime/high-rise-invasion' },
     { resultado: 'Hunter x Hunter', url: '../lista-anime/hunter-x-hunter' },
     { resultado: 'I Was Reincarnated as the 7th Prince so I Can Take My Time Perfecting My Magical Ability', url: '../lista-anime/i-was-reincarnated-as-the-7th-prince-so-i-can-take-my-time-perfecting-my-magical-ability' },
     { resultado: 'Ijiranaide, Nagatoro-san', url: '../lista-anime/ijiranaide-nagatoro-san' },
